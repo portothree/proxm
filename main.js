ce0c36216a8fb23ed67ebab001d5810f1b056250
Gustavo Porto <gustavoporto@ya.ru>
2021-12-29 21:13:03 +0000

    feat: add colorscheme

--- main.js
+++ main.js
@@ -1,37 +1,79 @@
 import blessed from 'blessed';
 
-const screen = blessed.screen({ smartCSR: true });
+const colorscheme = {
+	highlight: '#ffa500',
+	background: '#000000',
+	headline: '#ffffff',
+};
 
-screen.title = 'Proxm';
+const screen = blessed.screen({ smartCSR: true, title: 'Proxm' });
 
-const box = blessed.box({
+const qemuOverviewData = [
+	{
+		netout: 5253293,
+		name: 'Pi-hole',
+		maxmem: 2147483648,
+		mem: 326185963,
+		netin: 15046780,
+		cpus: 2,
+		uptime: 216958,
+		disk: 0,
+		maxdisk: 34359738368,
+		status: 'running',
+		vmid: 100,
+		diskread: 0,
+		pid: 2294,
+		diskwrite: 0,
+		cpu: 0.00088086718505653,
+	},
+	{
+		disk: 0,
+		maxdisk: 68719476736,
+		uptime: 86603,
+		balloon_min: 2147483648,
+		netin: 5121438,
+		cpus: 2,
+		maxmem: 4294967296,
+		mem: 2509918749,
+		netout: 298760,
+		name: 'k8s-1',
+		shares: 1000,
+		cpu: 0.0603732816834898,
+		pid: 1324639,
+		diskwrite: 0,
+		diskread: 0,
+		vmid: 101,
+		status: 'running',
+	},
+];
+
+const qemuOverviewBox = blessed.box({
+	parent: screen,
 	top: 'center',
 	left: 'center',
 	width: '50%',
 	height: '50%',
-	content: 'Hello {bold}world{/bold}!',
+	content: 'QEMU virtual machine index',
 	tags: true,
 	border: {
 		type: 'line',
 	},
 	style: {
-		fg: 'white',
-		bg: 'magenta',
+		fg: colorscheme.headline,
+		bg: colorscheme.background,
 		border: {
-			fg: '#f0f0f0',
+			fg: colorscheme.highlight,
 		},
 		hover: {
-			bg: 'green',
+			bg: colorscheme.headline,
 		},
 	},
 });
-
-screen.append(box);
 
 screen.key(['escape', 'q', 'C-c'], (ch, key) => {
 	return process.exit(0);
 });
 
-box.focus();
+qemuOverviewBox.focus();
 
 screen.render();
